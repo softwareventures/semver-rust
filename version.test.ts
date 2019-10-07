499555b41b6d08c6268f5143492ecc9e4b8ebd63
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-10-07 02:23:13 +0100

    test(version): add test for withoutMetadata function

--- version.test.ts
+++ version.test.ts
@@ -1,5 +1,5 @@
 import test from "ava";
-import {format} from "./version";
+import {format, withoutMetadata} from "./version";
 
 test("format", t => {
     t.is(format({major: 1, minor: 2, patch: 3, pre: [], build: []}), "1.2.3");
@@ -7,4 +7,13 @@ test("format", t => {
     t.is(format({major: 0, minor: 9, patch: 2, pre: [], build: ["softwareventures"]}), "0.9.2+softwareventures");
     t.is(format({major: 5, minor: 2, patch: 242, pre: ["2342", "9852", "beta"], build: ["complico", "8", "9", "5"]}),
         "5.2.242-2342.9852.beta+complico.8.9.5");
+});
+
+test("withoutMetadata", t => {
+    t.deepEqual(withoutMetadata({major: 1, minor: 2, patch: 3, pre: [], build: []}),
+        {major: 1, minor: 2, patch: 3, pre: [], build: []});
+    t.deepEqual(withoutMetadata({major: 2, minor: 3, patch: 4, pre: ["2", "development"], build: []}),
+        {major: 2, minor: 3, patch: 4, pre: [], build: []});
+    t.deepEqual(withoutMetadata({major: 0, minor: 9, patch: 2, pre: [], build: ["softwareventures"]}),
+        {major: 0, minor: 9, patch: 2, pre: [], build: []});
 });
